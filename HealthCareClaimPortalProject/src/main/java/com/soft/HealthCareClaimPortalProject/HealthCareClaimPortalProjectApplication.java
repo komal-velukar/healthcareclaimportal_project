22b0e3b5de003b52cfe52cc32a5069358c57fabc
komal velukar <velukark@gmail.com>
2025-10-31 12:15:23 +0530

    health care claim portal project

---
+++ HealthCareClaimPortalProject/src/main/java/com/soft/HealthCareClaimPortalProject/HealthCareClaimPortalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.soft.HealthCareClaimPortalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCareClaimPortalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCareClaimPortalProjectApplication.class, args);
+	}
+
+}
